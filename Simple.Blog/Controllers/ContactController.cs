582651dbe7b8ebd90566b7ca93332af29c48b516
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2023-10-15 11:26:01 +0545

    controller added

---
+++ Simple.Blog/Controllers/ContactController.cs
@@ -0,0 +1,60 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoRepository;
+using Simple.Blog.Models;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace Simple.Blog.Controllers
+{
+    public class ContactController:SurfaceController
+    {
+        private readonly MongoClient Client;
+        private readonly IMongoDatabase DB;
+
+        public ContactController(
+            IConfiguration configuration,
+            IUmbracoContextAccessor umbracoContextAccessor,
+            IUmbracoDatabaseFactory databaseFactory,
+            ServiceContext services,
+            AppCaches appCaches,
+            IProfilingLogger profilingLogger,
+            IPublishedUrlProvider publishedUrlProvider)
+            : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+            Client = new MongoClient(configuration["MyKey"]);
+            DB = Client.GetDatabase("simpleblog");
+        }
+
+        [HttpPost]
+        public IActionResult CreateContactMessage(ContactViewModel model)
+        {
+            try
+            {
+                if (model == null)
+                {
+                    return CurrentUmbracoPage();
+                }
+                var collection = DB.GetCollection<ContactViewModel>("Contact");
+                // var contactMessage = contactRepository.Add(model);
+                collection.InsertOne(model);
+                //ViewBag.Contact = contactMessage;
+                return RedirectToCurrentUmbracoPage();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
---
+++ Simple.Blog/Models/ContactViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoRepository;
+using System.ComponentModel.DataAnnotations;
+
+namespace Simple.Blog.Models
+{
+    public class ContactViewModel
+    {
+
+        public ObjectId Id { get; set; }
+        [Required]
+        [StringLength(200, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(2500, MinimumLength = 10)]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
